20867e8c7879c0ae92af279829c5c70d8bbc537d
Duncan T. Asiimwe <92824178+duncmv@users.noreply.github.com>
2025-10-24 23:52:25 +0300

    Delete api directory

--- api/src/routes/health.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { healthHandler } from '../controllers/health.controller';
-
-const router = Router();
-
-router.get('/health', healthHandler);
-
-export default router;
--- api/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import healthRoutes from './health.routes';
-
-const router = Router();
-
-router.use(healthRoutes);
-
-export default router;
